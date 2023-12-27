2b34abdd5890ab46b4a922f53b388e50704a7e24
Shiboka <d.aubie@outlook.com>
2023-12-27 15:21:16 -0400

    works on all files in a folder now

--- xmledit.js
+++ xmledit.js
@@ -1,29 +1,41 @@
-const fs = require('fs')
-const cheerio = require('cheerio')
+const fs = require('fs');
+const path = require('path');
+const cheerio = require('cheerio');
 const readline = require('node:readline').createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-readline.question("What file would you like to edit? ", file => {
-    fs.readFile(file, 'utf8', (err, data) => {
-        if(err) throw err;
-        var $ = cheerio.load(data, { xmlMode: true, decodeEntities: false });
+readline.question("What folder would you like to edit? ", folder => {
+    readline.question("What skill id would you like to edit? ", id => {
+        readline.question("What attribute would you like to change? ", attribute => {
+            readline.question("What would you like to change the value to? ", value => {
+                const dirPath = path.join(__dirname, folder);
 
-        readline.question("What skill id would you like to edit? ", id => {
-            readline.question("What attribute would you like to change? ", attribute => {
-                readline.question("What would you like to change the value to? ", value => {
-                    $('SkillData').find('Skill').each((i, element) => {
-                        if($(element).attr('id') == id) {
-                            $(element).attr(attribute, value);
-                            console.log(`Changed skill id ${id} ${attribute}="${value}"`)
-                        }
+                fs.readdir(dirPath, (err, files) => {
+                    if(err) throw err;
+
+                    files.forEach(file => {
+                        const filePath = path.join(dirPath, file);
+
+                        fs.readFile(filePath, 'utf8', (err, data) => {
+                            if(err) throw err;
+                            var $ = cheerio.load(data, { xmlMode: true, decodeEntities: false });
+
+                            $('SkillData').find('Skill').each((i, element) => {
+                                if($(element).attr('id') == id) {
+                                    $(element).attr(attribute, value);
+                                    console.log(`Changed skill id ${id} ${attribute}="${value}" in file: ${file}`)
+                                }
+                            });
+                
+                            fs.writeFile(filePath, $.xml(), err => {if(err) throw err});
+                        });
                     });
-        
-                    fs.writeFile(file, $.xml(), err => {if(err) throw err});
-                    readline.close();
                 });
-            })
+
+                readline.close();
+            });
         });
     });
 });
