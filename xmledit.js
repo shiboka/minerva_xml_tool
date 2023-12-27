31504058ddee9c798e4c825b9937a3f1a61b54e7
Shiboka <d.aubie@outlook.com>
2023-12-27 16:16:42 -0400

    added support for conf file

--- xmledit.js
+++ xmledit.js
@@ -2,26 +2,21 @@ const fs = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
 
-if(process.argv.length != 6) {
-    console.error('Error: 4 arguments are required:\nFolder\nSkill ID\nAttribute\nValue');
-    process.exit(1);
-}
-
-const folder = process.argv[2];
-const id = process.argv[3];
-const attribute = process.argv[4];
-const value = process.argv[5];
-
-const dirPath = path.join(__dirname, folder);
-
-fs.readdir(dirPath, (err, files) => {
-    if(err) throw err;
-
+function editFiles(err, files, dirPath) {
+    if(err) {
+        console.error(`Error: Could not open directory: ${dirPath}`);
+        process.exit(1);
+    };
+    
     files.forEach(file => {
         const filePath = path.join(dirPath, file);
 
         fs.readFile(filePath, 'utf8', (err, data) => {
-            if(err) throw err;
+            if(err) {
+                console.error(`Error: Could not open file: ${filePath}`)
+                process.exit(1);
+            }
+            
             var $ = cheerio.load(data, { xmlMode: true, decodeEntities: false });
 
             $('SkillData').find('Skill').each((i, element) => {
@@ -34,4 +29,28 @@ fs.readdir(dirPath, (err, files) => {
             fs.writeFile(filePath, $.xml(), err => { if(err) throw err });
         });
     });
+}
+
+if(process.argv.length != 6) {
+    console.error('Error: 4 arguments are required:\nFolder\nSkill ID\nAttribute\nValue');
+    process.exit(1);
+}
+
+const option = process.argv[2];
+const id = process.argv[3];
+const attribute = process.argv[4];
+const value = process.argv[5];
+
+fs.readFile('xmledit.json', 'utf8', (err, data) => {
+    if(err) {
+        console.error("Error: Could not open conf file xmledit.json")
+        process.exit(1);
+    }
+
+    const conf = JSON.parse(data);
+    const datasheetPath = conf[option].datasheet;
+    const databasePath = conf[option].database;
+
+    fs.readdir(datasheetPath, (err, files) => editFiles(err, files, datasheetPath));
+    fs.readdir(databasePath, (err, files) => editFiles(err, files, databasePath));
 });
