673784f3b10819745d9941f77222891b8c23e825
Shiboka <d.aubie@outlook.com>
2023-12-27 15:37:10 -0400

    changed from questions to command line arguments

--- xmledit.js
+++ xmledit.js
@@ -1,41 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
-const readline = require('node:readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
 
-readline.question("What folder would you like to edit? ", folder => {
-    readline.question("What skill id would you like to edit? ", id => {
-        readline.question("What attribute would you like to change? ", attribute => {
-            readline.question("What would you like to change the value to? ", value => {
-                const dirPath = path.join(__dirname, folder);
+if(process.argv.length != 6) {
+    console.error('Error: 4 arguments are required\nFolder\nSkill ID\nAttribute\nValue');
+    process.exit(1);
+}
 
-                fs.readdir(dirPath, (err, files) => {
-                    if(err) throw err;
+const folder = process.argv[2];
+const id = process.argv[3];
+const attribute = process.argv[4];
+const value = process.argv[5];
 
-                    files.forEach(file => {
-                        const filePath = path.join(dirPath, file);
+const dirPath = path.join(__dirname, folder);
 
-                        fs.readFile(filePath, 'utf8', (err, data) => {
-                            if(err) throw err;
-                            var $ = cheerio.load(data, { xmlMode: true, decodeEntities: false });
+fs.readdir(dirPath, (err, files) => {
+    if(err) throw err;
 
-                            $('SkillData').find('Skill').each((i, element) => {
-                                if($(element).attr('id') == id) {
-                                    $(element).attr(attribute, value);
-                                    console.log(`Changed skill id ${id} ${attribute}="${value}" in file: ${file}`)
-                                }
-                            });
-                
-                            fs.writeFile(filePath, $.xml(), err => {if(err) throw err});
-                        });
-                    });
-                });
+    files.forEach(file => {
+        const filePath = path.join(dirPath, file);
 
-                readline.close();
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if(err) throw err;
+            var $ = cheerio.load(data, { xmlMode: true, decodeEntities: false });
+
+            $('SkillData').find('Skill').each((i, element) => {
+                if($(element).attr('id') == id) {
+                    $(element).attr(attribute, value);
+                    console.log(`Changed skill id ${id} ${attribute}="${value}" in file: ${file}`)
+                }
             });
+
+            fs.writeFile(filePath, $.xml(), err => { if(err) throw err });
         });
     });
 });
