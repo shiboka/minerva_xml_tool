63d6789355452a32107aa9c85cc3b0b3a54132d1
Shiboka <d.aubie@outlook.com>
2023-12-28 10:07:24 -0400

    finished implementing new features

---
+++ README.md
@@ -0,0 +1,13 @@
+# Minerva XML Tool
+Tool for editing Tera xml files, only supports editing skills currently. There is an example config file for Warrior in the conf folder.
+
+Usage:
+
+`node xmltool.js [conf category] [conf file] [skill id] [apply linked skills y/n (optional)] [attributes]`
+
+Edits just this one skill with the given values:
+`node xmltool.js skill warrior 10100 totalAtk="100" mp="50"`
+`node xmltool.js skill warrior 10100 n totalAtk="100" mp="50"`
+
+Edits this skill, and applies modifiers to linked skills (specified in the conf):
+`node xmltool.js skill warrior 10100 y totalAtk="100" mp="50"`
--- xmltool.js
+++ xmltool.js
@@ -2,16 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
 
+/***********************************************/
+/* Process and validate command line arguments */
+/***********************************************/
 if(process.argv.length < 7) {
-    console.error('Error: At least 5 arguments are required:\nConf category i.e. skill\nConf file i.e. warrior\nSkill id: i.e. 10100\nChain skills: i.e. y/n\nValues: i.e. totalAtk="100" hp="100"');
+    console.error('Error: At least 5 arguments are required:\nConf category i.e. skill\nConf file i.e. warrior\nSkill id: i.e. 10100\nSkill link: y/n\nAttributes: i.e. totalAtk="100" mp="100"');
     process.exit(1);
 }
 
 const confCategory = process.argv[2].toLowerCase();
 const confFile = process.argv[3].toLowerCase();
-const id = process.argv[4];
-const chain = process.argv[5].toLowerCase();
-const values = process.argv.slice(6).map(value => {
+const skillId = process.argv[4];
+let skillLink = process.argv[5].toLowerCase();
+let values;
+
+if(skillLink == 'y' || skillLink == 'n') {
+    values = process.argv.slice(6)
+} else {
+    values = process.argv.slice(5);
+    skillLink = 'n';
+}
+
+values = values.map(value => {
     if(!value.includes('=')) {
         console.error(`Error: Invalid value given: ${value}`);
         process.exit(1);
@@ -25,32 +37,44 @@ if(confCategory != 'skill') {
     process.exit(1);
 }
 
-function editFiles(err, files, dirPath) {
-    if(err) {
-        console.error(`Error: Could not open directory: ${dirPath}`);
-        process.exit(1);
-    };
-    
-    files.forEach(file => {
-        const filePath = path.join(dirPath, file);
+/**********************************************/
 
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if(err) {
-                console.error(`Error: Could not open file: ${filePath}`)
-                process.exit(1);
+/************************/
+/* Function Definitions */
+/************************/
+function editSkill($, file, id, attribute, value) {
+    $('SkillData').find('Skill').each((i, e) => {
+        if($(e).attr('id') == id) {
+            if(attribute == "mp" || attribute == "hp" || attribute == "anger") {
+                $(e).find('TargetingList').each((i, e) => {
+                    $(e).find('Targeting').each((i,e) => {
+                        $(e).find('Cost').each((i,e) => {
+                            $(e).attr(attribute, value);
+                        });
+                    });
+                });
+
+                $(e).find('Precondition').each((i, e) => {
+                    $(e).find('Cost').each((i, e) => {
+                        $(e).attr(attribute, value);
+                    });
+                });
+            } else if(attribute == 'coolTime') {
+                $(e).find('Precondition').each((i, e) => {
+                    $(e).attr(attribute, value);
+                });
+            } else if(attribute == 'startCancelEndTime' || attribute == 'rearCancelStartTime' || attribute == 'moveCancelStartTime') {
+                $(e).find('Action').each((i, e) => {
+                    $(e).find('Cancel').each((i, e) => {
+                        $(e).attr(attribute, value);
+                    });
+                });
+            } else if(attribute == "totalAtk" || attribute == "timeRate" || attribute == 'attackRange' || attribute == 'pushTarget') {
+                $(e).attr(attribute, value);
             }
 
-            var $ = cheerio.load(data, { xmlMode: true, decodeEntities: false });
-
-            $('SkillData').find('Skill').each((i, element) => {
-                if($(element).attr('id') == id) {
-                    $(element).attr(attribute, value);
-                    console.log(`Changed skill id ${id} ${attribute}="${value}" in file: ${file}`)
-                }
-            });
-
-            fs.writeFile(filePath, $.xml(), err => { if(err) throw err });
-        });
+            console.log(`Changed skill id ${id} ${attribute}="${value}" in file: ${file}`);
+        }
     });
 }
 
@@ -72,52 +96,29 @@ function editSkills(err, files, dir, conf) {
             var $ = cheerio.load(data, { xmlMode: true, decodeEntities: false });
 
             values.forEach(value => {
-                $('SkillData').find('Skill').each((i, e) => {
-                    if($(e).attr('id') == id) {
-                        if(value[0] == "mp" || value[0] == "hp" || value[0] == "anger") {
-                            $(e).find('TargetingList').each((i, e) => {
-                                $(e).find('Targeting').each((i,e) => {
-                                    $(e).find('Cost').each((i,e) => {
-                                        console.log('TargetingList/Targeting/Cost:');
-                                        console.log(value[0]);
-                                        console.log($(e).attr(value[0]));
-                                    });
-                                });
-                            });
+                editSkill($, file, skillId, value[0], value[1]);
 
-                            $(e).find('Precondition').each((i, e) => {
-                                $(e).find('Cost').each((i, e) => {
-                                    console.log('Precondition/Cost:');
-                                    console.log(value[0]);
-                                    console.log($(e).attr(value[0]));
-                                });
-                            });
-                        } else if(value[0] == 'coolTime') {
-                            $(e).find('Precondition').each((i, e) => {
-                                console.log('Precondition:');
-                                console.log(value[0]);
-                                console.log($(e).attr(value[0]));
-                            });
-                        } else if(value[0] == 'startCancelEndTime' || value[0] == 'rearCancelStartTime' || value[0] == 'moveCancelStartTime') {
-                            $(e).find('Action').each((i, e) => {
-                                $(e).find('Cancel').each((i, e) => {
-                                    console.log('Action/Cancel:')
-                                    console.log(value[0]);
-                                    console.log($(e).attr(value[0]));
-                                });
-                            });
-                        } else if(value[0] == "totalAtk" || value[0] == "timeRate" || value[0] == 'attackRange' || value[0] == 'pushTarget') {
-                            console.log('Skill:');
-                            console.log(value[0]);
-                            console.log($(e).attr(value[0]));
-                        }
-                    }
-                });
+                if(skillLink == 'y') {
+                    conf.Skills[skillId].forEach((skill, i) => {
+                        const baseFloat = parseFloat(value[1]);
+                        const modFloat = parseFloat(conf.Attributes[value[0]][skillId][i]);
+                        const modifiedValue = baseFloat + baseFloat * modFloat;
+                        
+                        editSkill($, file, skill, value[0], modifiedValue)
+                    });
+                }
             });
+
+            fs.writeFile(filePath, $.xml(), err => { if(err) throw err });
         });
     });
 }
 
+/*************************************************/
+
+/**************************************/
+/* Read conf file and begin execution */
+/**************************************/
 fs.readFile(`conf/${confCategory}/${confFile}.json`, 'utf8', (err, data) => {
     if(err) {
         console.error(`Error: conf/${confCategory}/${confFile}.json does not exist.`);
@@ -128,9 +129,10 @@ fs.readFile(`conf/${confCategory}/${confFile}.json`, 'utf8', (err, data) => {
     const datasheetPath = conf.Datasheet;
     const databasePath = conf.Database;
 
-    if(chain == "y") {
-        if(conf.Skills[id] == undefined) {
-            console.error(`Error: Invalid conf. Y given but no skill chain specified for Skill ID "${id}" in the conf.`);
+    // Validate conf file if Y given
+    if(skillLink == "y") {
+        if(conf.Skills[skillId] == undefined) {
+            console.error(`Error: Invalid conf. Y given but no skill chain specified for Skill ID "${skillId}" in the conf.`);
             process.exit(1);
         }
 
@@ -140,33 +142,20 @@ fs.readFile(`conf/${confCategory}/${confFile}.json`, 'utf8', (err, data) => {
                 process.exit(1);
             }
 
-            if(conf.Attributes[value[0]][id] == undefined) {
-                console.error(`Error: Invalid conf. Y given but no Skill ID "${id}" specified for Attribute "${value[0]}" in the conf.`)
+            if(conf.Attributes[value[0]][skillId] == undefined) {
+                console.error(`Error: Invalid conf. Y given but no Skill ID "${skillId}" specified for Attribute "${value[0]}" in the conf.`)
                 process.exit(1);
             }
 
-            if(conf.Attributes[value[0]][id].length != conf.Skills[id].length) {
+            if(conf.Attributes[value[0]][skillId].length != conf.Skills[skillId].length) {
                 console.error(`Error: Invalid conf. Y given but length of list for "${value[0]}" does not equal length of list for Skill ID "${id}" in the conf.`);
                 process.exit(1);
             }
         });
     }
 
-    //fs.readdir(datasheetPath, (err, files) => editSkills(err, files, datasheetPath, conf));
+    fs.readdir(datasheetPath, (err, files) => editSkills(err, files, datasheetPath, conf));
+    fs.readdir(databasePath, (err, files) => editSkills(err, files, databasePath, conf));
 });
 
-/*
-fs.readFile('xmledit.json', 'utf8', (err, data) => {
-    if(err) {
-        console.error("Error: Could not open conf file xmledit.json")
-        process.exit(1);
-    }
-
-    const conf = JSON.parse(data);
-    const datasheetPath = conf[option].datasheet;
-    const databasePath = conf[option].database;
-
-    fs.readdir(datasheetPath, (err, files) => editFiles(err, files, datasheetPath));
-    fs.readdir(databasePath, (err, files) => editFiles(err, files, databasePath));
-});
-*/
+/*****************************************************/
