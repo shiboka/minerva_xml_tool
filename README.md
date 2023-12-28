47892b2a6047c07d348491075699424a422a8703
Shiboka <d.aubie@outlook.com>
2023-12-28 19:40:30 -0400

    added support for NpcData

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Minerva XML Tool
-Tool for editing Tera xml files, only supports editing skills currently. There is an example config file for Warrior in the conf folder.
+Tool for editing Tera xml files, currently supports SkillData and NpcData. There are example configs in the conf folder.
+
+
+### SkillData
 
 Usage:
 
@@ -18,3 +21,28 @@ Edits this skill, and applies modifiers to linked skills (specified in the conf)
 Supported attributes are:
 
 totalAtk, timeRate, attackRange, coolTime, mp, hp, anger, startCancelEndTime, rearCancelStartTime, moveCancelStartTime.
+
+
+### NpcData
+
+Usage:
+
+`node xmltool.js [conf category] [conf file] [mob id/size/elite/nothing for all] [attributes]`
+
+Edits just one mob of the given id (formated as: huntingZoneId-npcId):
+
+`node xmltool.js area 1 3-300811 maxHp="1000"`
+
+`node xmltool.js area 1 3-300811 maxHp="+0.2"`
+
+Edits all large monsters in the given area to have +20% hp and +20% def:
+
+`node xmltool.js area 1 large maxHp="+0.2" def="+0.2"`
+
+Edits ALL monsters in the given area to have +20% hp and +20% def:
+
+`node xmltool.js area 1 large maxHp="+0.2 def="+0.2""`
+
+Supported attributes are:
+
+maxHp, def, atk.
--- xmltool.js
+++ xmltool.js
@@ -5,8 +5,8 @@ const cheerio = require('cheerio');
 /***********************************************/
 /* Process and validate command line arguments */
 /***********************************************/
-if(process.argv.length < 7) {
-    console.error('Error: At least 5 arguments are required:\nConf category i.e. skill\nConf file i.e. warrior\nSkill id: i.e. 10100\nSkill link: y/n\nAttributes: i.e. totalAtk="100" mp="100"');
+if(process.argv.length < 6) {
+    console.error('Error: At least 4 arguments are required.');
     process.exit(1);
 }
 
@@ -16,11 +16,23 @@ const skillId = process.argv[4];
 let skillLink = process.argv[5].toLowerCase();
 let values;
 
-if(skillLink == 'y' || skillLink == 'n') {
-    values = process.argv.slice(6)
+if(confCategory == 'skill') {
+    if(skillLink == 'y' || skillLink == 'n') {
+        values = process.argv.slice(6)
+    } else {
+        values = process.argv.slice(5);
+        skillLink = 'n';
+    }
+} else if(confCategory == 'area') {
+    if(skillId.includes('=')) {
+        values = process.argv.slice(4);
+        skillId = 'all';
+    } else {
+        values = process.argv.slice(5)
+    }
 } else {
-    values = process.argv.slice(5);
-    skillLink = 'n';
+    console.error(`Error: ${confCategory} is an invalid category.`);
+    process.exit(1);
 }
 
 values = values.map(value => {
@@ -31,11 +43,6 @@ values = values.map(value => {
 
     return value.split('=');
 });
-
-if(confCategory != 'skill') {
-    console.error(`Error: ${confCategory} is an invalid category.`);
-    process.exit(1);
-}
 
 /**********************************************/
 
@@ -137,6 +144,109 @@ function editSkills(err, files, dir, conf) {
     });
 }
 
+function editNpcStat($, e, attribute, value, area) {
+    let modifiedValue;
+
+    if(area && (value[0] != '+' && value[0] != '-')) {
+        console.error(`Error: Only percentages are allowed for area edits.`)
+        process.exit(1);
+    }
+
+    $(e).find('Stat').each((i, e) => {
+        if(value[0] == '+' || value[0] == '-') {
+            const base = parseFloat($(e).attr(attribute));
+            const modifier = parseFloat(value);
+            modifiedValue = base + base * modifier;
+        } else {
+            modifiedValue = value;
+        }
+
+        if(attribute == 'atk' || attribute == 'def') {
+            if($(e).attr(attribute) != undefined) {
+                modifiedValue = parseInt(modifiedValue)
+                $(e).attr(attribute, modifiedValue);
+            }
+        } else {
+            if($(e).attr(attribute) != undefined) {
+                modifiedValue = parseFloat(modifiedValue).toFixed(2);
+                $(e).attr(attribute, modifiedValue);
+            }
+        }
+    });
+
+    return modifiedValue;
+}
+
+function editArea(err, files, dir) {
+    if(err) {
+        console.error(`Error: Could not open directory: ${dir}`);
+        process.exit(1);
+    };
+
+    files.forEach(file => {
+        const filePath = path.join(dir, file);
+
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if(err) {
+                console.error(`Error: Could not open file: ${filePath}`)
+                process.exit(1);
+            }
+
+            var $ = cheerio.load(data, { xmlMode: true, decodeEntities: false });
+
+            values.forEach(value => {
+                if(value[0] != 'maxHp' && value[0] != 'atk' && value[0] != 'def') {
+                    console.error(`Error: Unsupported attribute ${value[0]}.`);
+                    process.exit(1);
+                }
+
+                $('NpcData').find('Template').each((i, e) => {
+                    let changed = false;
+
+                    if(skillId == 'all') {
+                        const modifiedValue = editNpcStat($, e, value[0], value[1], true);
+                        console.log(`Changed npc ${$(e).attr('id')} ${value[0]}="${modifiedValue}" in file: ${file}`);
+                    } else if(skillId == 'small') {
+                        if($(e).attr('size') == 'small') {
+                            const modifiedValue = editNpcStat($, e, value[0], value[1], true);
+                            console.log(`Changed npc ${$(e).attr('id')} ${value[0]}="${modifiedValue}" in file: ${file}`);
+                        }
+                    } else if(skillId == 'medium') {
+                        if($(e).attr('size') == 'medium') {
+                            const modifiedValue = editNpcStat($, e, value[0], value[1], true);
+                            console.log(`Changed npc ${$(e).attr('id')} ${value[0]}="${modifiedValue}" in file: ${file}`);
+                        }
+                    } else if(skillId == 'large') {
+                        if($(e).attr('size') == 'large') {
+                            const modifiedValue = editNpcStat($, e, value[0], value[1], true);
+                            console.log(`Changed npc ${$(e).attr('id')} ${value[0]}="${modifiedValue}" in file: ${file}`);
+                        }
+                    } else if(skillId == 'elite') {
+                        if($(e).attr('elite').toLowerCase() == 'true') {
+                            const modifiedValue = editNpcStat($, e, value[0], value[1], true);
+                            console.log(`Changed npc ${$(e).attr('id')} ${value[0]}="${modifiedValue}" in file: ${file}`);
+                        }
+                    } else {
+                        if(!skillId.includes('-')) {
+                            console.error(`Error: Invalid id ${skillId}.`);
+                            process.exit(1);
+                        }
+                        
+                        const ids = skillId.split('-');
+
+                        if($('NpcData').attr('huntingZoneId') == ids[0] && $(e).attr('id') == ids[1]) {
+                            const modifiedValue = editNpcStat($, e, value[0], value[1], false);
+                            console.log(`Changed npc ${$(e).attr('id')} ${value[0]}="${modifiedValue}" in file: ${file}`);
+                        }
+                    }
+                });
+            });
+
+            fs.writeFile(filePath, $.xml(), err => { if(err) throw err });
+        });
+    });
+}
+
 /*************************************************/
 
 /**************************************/
@@ -152,33 +262,38 @@ fs.readFile(`conf/${confCategory}/${confFile}.json`, 'utf8', (err, data) => {
     const datasheetPath = conf.Datasheet;
     const databasePath = conf.Database;
 
-    // Validate conf file if Y given
-    if(skillLink == "y") {
-        if(conf.Skills[skillId] == undefined) {
-            console.error(`Error: Invalid conf. Y given but no skill chain specified for Skill ID "${skillId}" in the conf.`);
-            process.exit(1);
+    if(confCategory == 'skill') {
+        // Validate conf file if Y given
+        if(skillLink == "y") {
+            if(conf.Skills[skillId] == undefined) {
+                console.error(`Error: Invalid conf. Y given but no skill chain specified for Skill ID "${skillId}" in the conf.`);
+                process.exit(1);
+            }
+
+            values.forEach(value => {
+                if(conf.Attributes[value[0]] == undefined) {
+                    console.error(`Error: Invalid conf. Y given but no Attribute "${value[0]}" specified in the conf.`);
+                    process.exit(1);
+                }
+
+                if(conf.Attributes[value[0]][skillId] == undefined) {
+                    console.error(`Error: Invalid conf. Y given but no Skill ID "${skillId}" specified for Attribute "${value[0]}" in the conf.`)
+                    process.exit(1);
+                }
+
+                if(conf.Attributes[value[0]][skillId].length != conf.Skills[skillId].length) {
+                    console.error(`Error: Invalid conf. Y given but length of list for "${value[0]}" does not equal length of list for Skill ID "${id}" in the conf.`);
+                    process.exit(1);
+                }
+            });
         }
 
-        values.forEach(value => {
-            if(conf.Attributes[value[0]] == undefined) {
-                console.error(`Error: Invalid conf. Y given but no Attribute "${value[0]}" specified in the conf.`);
-                process.exit(1);
-            }
-
-            if(conf.Attributes[value[0]][skillId] == undefined) {
-                console.error(`Error: Invalid conf. Y given but no Skill ID "${skillId}" specified for Attribute "${value[0]}" in the conf.`)
-                process.exit(1);
-            }
-
-            if(conf.Attributes[value[0]][skillId].length != conf.Skills[skillId].length) {
-                console.error(`Error: Invalid conf. Y given but length of list for "${value[0]}" does not equal length of list for Skill ID "${id}" in the conf.`);
-                process.exit(1);
-            }
-        });
+        fs.readdir(datasheetPath, (err, files) => editSkills(err, files, datasheetPath, conf));
+        fs.readdir(databasePath, (err, files) => editSkills(err, files, databasePath, conf));
+    } else if(confCategory == 'area') {
+        fs.readdir(datasheetPath, (err, files) => editArea(err, files, datasheetPath));
+        fs.readdir(databasePath, (err, files) => editArea(err, files, databasePath));    
     }
-
-    fs.readdir(datasheetPath, (err, files) => editSkills(err, files, datasheetPath, conf));
-    fs.readdir(databasePath, (err, files) => editSkills(err, files, databasePath, conf));
 });
 
 /*****************************************************/ 
