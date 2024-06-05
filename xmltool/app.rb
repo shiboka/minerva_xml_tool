c56ac44d933cf6174e252a9fef9094e44787e2d6
Shiboka <d.aubie@outlook.com>
2024-06-04 22:57:45 -0300

    added help messages

--- xmltool/app.rb
+++ xmltool/app.rb
@@ -20,6 +20,19 @@ module XMLTool
     end
 
     desc "skill CLASS ID ATTRIBUTES", "modify skill"
+    long_desc <<-LONGDESC
+      Will modify the skill with the given ID for the given class.
+
+      The ATTRIBUTES argument should be a list of key-value pairs separated by an equal sign. For example: totalAtk=100.
+
+      The command will prompt you to apply linked skills. If you choose to apply linked skills, the command will modify the linked skills specified in the config as well.
+
+      Example:
+
+      To modify the totalAtk of the warrior auto-attack skill::
+
+      ruby xmltool.rb skill warrior 10100 totalAtk=100
+    LONGDESC
     def skill(clazz, id, *attrs_raw)
       link = ask("Do you want to apply linked skills? (Y/N)").downcase
       attrs = AttrUtils.parse_attrs(attrs_raw)
@@ -47,6 +60,31 @@ module XMLTool
     end
 
     desc "area NAME MOB ATTRIBUTES", "modify area"
+long_desc <<-LONGDESC
+Will modify the area of the given name for the given mob.
+
+MOB can be a mob ID or a size, i.e. small, medium, large, elite, or all.
+
+The ATTRIBUTES argument should be a list of key-value pairs separated by an equal sign. For example: maxHp=100.
+
+Examples:
+
+If your config defined an area oblivion_woods within an area arcadia, you could modify the maxHp attribute of an Oblivion Woods Basilisk like this:
+
+ruby xmltool.rb area arcadia/oblivion_woods 300811 maxHp=100
+
+To modify mobs of a specific size, you can use the size instead of the mob ID. For example, to modify all elite mobs in the area:
+
+ruby xmltool.rb area arcadia/oblivion_woods elite maxHp=100
+
+If you want to modify all mobs in the area, you can use the all keyword. For example, to modify all mobs in the area:
+
+ruby xmltool.rb area arcadia/oblivion_woods all maxHp=100
+
+If your config defined only a top-level area: fey_forest. You can modify the respawnTime of all small mobs in the area like this:
+
+ruby xmltool.rb area fey_forest small respawnTime=100
+LONGDESC
     def area(name, mob, *attrs_raw)
       attrs = AttrUtils.parse_attrs(attrs_raw)
       areas = name.split("/")
@@ -73,6 +111,21 @@ module XMLTool
     end
 
     desc "stats CLASS RACE ATTRIBUTES", "modify player stats"
+    long_desc <<-LONGDESC
+      Will modify the stats of the given class and (optionally) race.
+
+      The ATTRIBUTES argument should be a list of key-value pairs separated by an equal sign. For example: maxMp=100.
+
+      Examples:
+
+      To modify the maxMp of all warrior races:
+
+      ruby xmltool.rb stats warrior all maxMp=100
+
+      To modify the maxMp of a specific archer race:
+
+      ruby xmltool.rb stats archer popori maxMp=100
+    LONGDESC
     def stats(clazz, race, *attrs_raw)
       attrs = AttrUtils.parse_attrs(attrs_raw)
 
@@ -90,6 +143,17 @@ module XMLTool
     end
 
     desc "config CLASS", "generate config for class"
+    long_desc <<-LONGDESC
+      Will generate a config file for the given class.
+
+      Config files are saved in the config/skill directory.
+
+      Example:
+
+      To generate a config file for the warrior class:
+
+      ruby xmltool.rb config warrior
+    LONGDESC
     def config(clazz)
       begin
         global_config = ConfigLoader.load_config("config/sources.yml")
