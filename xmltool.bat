e21389c64ec1ace52f383898bd58f09ef7b4b98b
Shiboka <d.aubie@outlook.com>
2024-06-06 08:12:37 -0300

    Update xmltool.bat and xmltool.sh, added variable for docker image

--- xmltool.bat
+++ xmltool.bat
@@ -1,10 +1,12 @@
 @echo off
+set IMAGE=ghcr.io/shiboka/xmltool:main
+
 set DATASHEET=C:/path/to/xmltool/data/Datasheet
 set DATABASE=C:/path/to/xmltool/data/Database
 set CONFIG=C:/path/to/xmltool/config
 
 REM Run the container
-docker run -e DATASHEET=%DATASHEET% -e DATABASE=%DATABASE% -e CONFIG=%CONFIG% -v %CONFIG_HOST%:%CONFIG_CONTAINER% -v %DATA_HOST%:%DATA_CONTAINER% -it xmltool %*
+docker run -e DATASHEET=%DATASHEET% -e DATABASE=%DATABASE% -e CONFIG=%CONFIG% -v %CONFIG_HOST%:%CONFIG_CONTAINER% -v %DATA_HOST%:%DATA_CONTAINER% -it %IMAGE% %*
 
 REM Run without Docker
 REM ruby xmltool.rb "$@"
--- xmltool.sh
+++ xmltool.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+export IMAGE=ghcr.io/shiboka/xmltool:main
+
 # sources
 export DATASHEET=/path/to/xmltool/data/Datasheet
 export DATABASE=/path/to/xmltool/data/Database
 export CONFIG=/path/to/xmltool/config
 
 # Run with Docker
-docker run -e DATASHEET=$DATASHEET -e DATABASE=$DATABASE -e CONFIG=$CONFIG -v $CONFIG:$CONFIG -v $DATASHEET:$DATASHEET -v $DATABASE:$DATABASE -it xmltool "$@"
+docker run -e DATASHEET=$DATASHEET -e DATABASE=$DATABASE -e CONFIG=$CONFIG -v $CONFIG:$CONFIG -v $DATASHEET:$DATASHEET -v $DATABASE:$DATABASE -it $IMAGE "$@"
 
 # Run without Docker
 #ruby xmltool.rb "$@"
